bc817593b28ca9ea2c119ee8bcadbdeddef5be8f
Pablo <flexdeveloper@gmgs-iMac.local>
2012-02-24 18:13:27 -0600

    Nuevas fuentes

--- main.pl
+++ main.pl
@@ -2,9 +2,11 @@
 use strict;
 use warnings;
 use WWW::Spider;
-use HTML::Parser;
+use Marpa::HTML qw(html);
+use Data::Inspect;
 
 sub main{
+  	my $insp = Data::Inspect->new;
 =begin
 	my $website = "http://en.wikipedia.org/wiki/Artificial_intelligence";
 	my $spider = new WWW::Spider;
@@ -28,19 +30,29 @@ sub main{
  	close (URL_FILE); 
 
  	print "I've just get the content\n";
+
+    my $old_title = '<title>Old Title</title>A little html text';
+    my $new_title = html(
+        \$old_title,
+        {   'title' => sub { return '<title>New Title</title>' }
+        }
+    );
+  	my $insp = Data::Inspect->new;
+  	$insp->p($new_title);
+    print $new_title,title;
 =cut
 
-  	my $parser = HTML::Parser->new(api_version => 3);
-  	$parser->handler( start => \&start_handler, "tagname,self");
-  	$parser->parse_file(shift || die) || die $!;
-  	print "\n";
-}
+   	#my $with_table = 'Text<table><tr><td>I am a cell</table> More Text';
+   	#my @attributes = Marpa::HTML::attributes($with_table);
+   	#$insp->p(@attributes);
 
-sub start_handler{
-    return if shift ne "title";
-    my $self = shift;
-    $self->handler(text => sub { print shift }, "artificial_intelligence.txt");
-    $self->handler(end  => sub { shift->eof if shift eq "title"; }, "tagname,self");
+   	my $with_table = 'Text<table><tr><td>I am a cell</table> More Text';
+    my %handlers_to_keep_only_tables = (
+        table  => sub { return Marpa::HTML::original() },
+        ':TOP' => sub { return \( join q{}, @{ Marpa::HTML::values() } ) }
+    );
+    my $only_table = html( \$with_table, \%handlers_to_keep_only_tables );
+    print $only_table.value;;
 }
 
 main(@ARGV);
